98ee53cdd7ea4c24c4a8c7fa2280480b53125290
xuckless <72529941+xuckless@users.noreply.github.com>
2020-12-30 20:20:35 +0530

    Add files via upload

---
+++ flaskblog/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+    SECRET_KEY = '6f5e8de647bb6e7d9a554a5d3739106d'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 'smtp.googlemail.com'
+    MAIL_USERNAME = 'blogfordinner'
+    MAIL_PASSWORD = 'aliwebapp.dine'  
